83e9d3c06340b0fe7ca977d2a23ef8c83e3166d8
leoneville <leoneville_@hotmail.com>
2024-07-20 21:12:29 -0300

    feat: Introduce Entity Framework Core with MySQL
    Set up Entity Framework Core with a MySQL database provider in the infrastructure layer. This lays the groundwork for persistence and data access capabilities within the application.

---
+++ src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
@@ -0,0 +1,18 @@
+﻿using CashFlow.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CashFlow.Infrastructure.DataAccess;
+public class CashFlowDbContext : DbContext
+{
+    public DbSet<Expense> Expenses { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var connectionString = "Server=localhost;Database=cashflowdb;Uid=admin;Pwd=@password123;";
+
+        var version = new Version(8, 4, 0);
+        var serverVersion = new MySqlServerVersion(version);
+
+        optionsBuilder.UseMySql(connectionString, serverVersion);
+    }
+}
