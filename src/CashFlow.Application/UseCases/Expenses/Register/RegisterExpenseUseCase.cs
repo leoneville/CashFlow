577619a0ec946e6449f09ca278cb8ee8c87de772
leoneville <leoneville_@hotmail.com>
2024-11-17 09:33:33 -0300

    Adds endpoint to get all expenses
    Implements the use case and controller action for retrieving all expenses.
    
    Includes necessary changes to the repository, application layer, and API endpoint.  Uses AutoMapper for mapping between data objects.

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -1,4 +1,5 @@
-﻿using CashFlow.Communication.Requests;
+﻿using AutoMapper;
+using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
 using CashFlow.Domain.Entities;
 using CashFlow.Domain.Enums;
@@ -12,34 +13,29 @@ public class RegisterExpenseUseCase : IRegisterExpenseUseCase
 {
     private readonly IExpensesRepository _repository;
     private readonly IUnityOfWork _unityOfWork;
+    private readonly IMapper _mapper;
 
-    public RegisterExpenseUseCase(IExpensesRepository repository, IUnityOfWork unityOfWork)
+    public RegisterExpenseUseCase(
+        IExpensesRepository repository,
+        IUnityOfWork unityOfWork,
+        IMapper mapper)
     {
         _repository = repository;
         _unityOfWork = unityOfWork;
+        _mapper = mapper;
     }
 
     public async Task<ResponseRegisteredExpenseJson> Execute(RequestRegisterExpenseJson request)
     {
         Validate(request);
 
-        var entity = new Expense
-        {
-            Amount = request.Amount,
-            Date = request.Date,
-            Description = request.Description,
-            Title = request.Title,
-            PaymentType = (PaymentType)request.PaymentType,
-        };
+        var entity = _mapper.Map<Expense>(request);
 
         await _repository.Add(entity);
 
         await _unityOfWork.Commit();
 
-        return new ResponseRegisteredExpenseJson
-        {
-            Title = entity.Title,
-        };
+        return _mapper.Map<ResponseRegisteredExpenseJson>(entity);
     }
 
     private static void Validate(RequestRegisterExpenseJson request)
